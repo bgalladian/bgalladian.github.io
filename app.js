4eb827357db9a1f1c91f158ea495135d91efc8e8
Bedig Galladian <bgalladian@gmail.com>
2017-03-31 14:34:39 -0400

    added loading page

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 $(window).on('load', _ => {
-    $('#preloader').delay(350).fadeOut('slow'); 
-    $('body').delay(350).css({'overflow':'visible'});
+    $('#preloader').delay(1000).fadeOut('slow');
+    $('body').delay(0).css({'overflow':'visible'});
 })
 
 console.log("If you like my code or have suggestions, please feel free to let me know!")
