ee3ab17fa05323e7f67dfac8076895c05547c156
Bedig Galladian <bgalladian@gmail.com>
2017-04-19 09:27:56 -0400

    lowered time delay for preloader

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 $(window).on('load', _ => {
-    $('#preloader').delay(1000).fadeOut('slow');
+    $('#preloader').delay(500).fadeOut('slow');
     $('body').delay(0).css({'overflow':'visible'});
 })
 
