eed0af5aec15ff8ac31101b51c16aa1764e21830
Bedig Galladian <bgalladian@gmail.com>
2017-04-23 13:38:10 -0400

    created materialize branch

--- app.js
+++
@@ -1,8 +0,0 @@
-$(window).on('load', _ => {
-    $('#preloader').delay(500).fadeOut('slow');
-    $('body').delay(0).css({'overflow':'visible'});
-})
-
-
-
-console.log("If you like my code or have suggestions, please feel free to let me know!")
