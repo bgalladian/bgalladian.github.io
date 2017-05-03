b91407593ed9987bf21df12651e4011c69a3412b
Bedig Galladian <bgalladian@gmail.com>
2017-05-03 12:26:37 -0400

    added preloader

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+$(window).on('load', _ => {
+    $('#preloader').delay(500).fadeOut('slow');
+    $('body').delay(0).css({'overflow':'visible'});
+})
+
 
 $(document).ready(function(e) {
 	$('.with-hover-text, .regular-link').click(function(e){
